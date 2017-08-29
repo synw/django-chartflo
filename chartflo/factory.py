bfc6733f216d4dc92556943f00e7406b07073d39
Win <synwe@yahoo.com>
2017-08-29 11:08:25 +0000

    Normalize variable names in factory and reorder functions #7

--- chartflo/factory.py
+++ chartflo/factory.py
@@ -3,18 +3,27 @@
 
 class ChartDataPack():
 
-    def _count_for_query(self, query, fieldpack):
+    def package(self, chart_id, data_label, dataset, legend=False):
+        return {'chart_id': chart_id, 'data_label': data_label, "dataset": dataset, "legend": legend}
+
+    def count(self, query, field=None, func=None):
+        pack = {}
+        if field is not None:
+            pack = {field: func}
+        return self._count_for_query(query, pack)
+
+    def _count_for_query(self, query, fieldchecks):
         dataset = {}
         counter = 0
-        for elem in query:
+        for object in query:
             commit = True
             modelname = query[0].__class__.__name__
             name = modelname
-            if len(fieldpack.keys()) > 0:
-                for fieldname in fieldpack.keys():
+            if len(fieldchecks.keys()) > 0:
+                for fieldname in fieldchecks.keys():
                     name = modelname + "." + fieldname
-                    fieldval = str(getattr(elem, fieldname))
-                    func = fieldpack[fieldname]
+                    fieldval = str(getattr(object, fieldname))
+                    func = fieldchecks[fieldname]
                     if func is not None:
                         if func(fieldval) is False:
                             commit = False
@@ -25,12 +34,3 @@ class ChartDataPack():
                 else:
                     dataset[name] = 1
         return counter
-
-    def package(self, chart_id, data_label, dataset, legend=False):
-        return {'chart_id': chart_id, 'data_label': data_label, "dataset": dataset, "legend": legend}
-
-    def count(self, query, field=None, func=None):
-        pack = {}
-        if field is not None:
-            pack = {field: func}
-        return self._count_for_query(query, pack)

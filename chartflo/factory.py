1f1a728faac698613d10985b4e044fc1f6ece06e
Win <synwe@yahoo.com>
2017-08-31 12:37:13 +0000

    Remove unused code from factory #7

--- chartflo/factory.py
+++ chartflo/factory.py
@@ -13,15 +13,11 @@ class ChartController():
         return self._count_for_query(query, pack)
 
     def _count_for_query(self, query, fieldchecks):
-        dataset = {}
         counter = 0
         for obj in query:
             commit = True
-            modelname = query[0].__class__.__name__
-            name = modelname
             if len(fieldchecks.keys()) > 0:
                 for fieldname in fieldchecks.keys():
-                    name = modelname + "." + fieldname
                     fieldval = str(getattr(obj, fieldname))
                     func = fieldchecks[fieldname]
                     if func is not None:
@@ -29,8 +25,4 @@ class ChartController():
                             commit = False
             if commit is True:
                 counter += 1
-                if name in dataset.keys():
-                    dataset[name] = dataset[name] + 1
-                else:
-                    dataset[name] = 1
         return counter

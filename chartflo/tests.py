81a0239812d01e9e876989d2334afe746e09f5da
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-09-07 12:02:38 +0300

    Add Vega Lite template test

--- chartflo/tests.py
+++ chartflo/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .views import ChartsView
 
 # Create your tests here.
+class TestVegaLiteChartsView(TestCase):
+    def setUpTestCase(self):
+        self.chart_view = ChartsView()
+
+        # Set Vega Lite as template engine
+        self.chart_view.engine = "vegalite"
+
+    def test_vega_lite_template(self):
+        # URL for Vega Lite chart URL
+        vega_lite_template_url = "chartflo/vegalite/chart.html"
+
+        # Get chart view template URL
+        chart_view_template_url = self.chart_view._get_template_url()
+
+        # Make sure Chart View URL matches Vega Lite chart URL
+        self.assertEqual(chart_view_template_url, vega_lite_template_url)

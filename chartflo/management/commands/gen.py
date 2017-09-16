2f2f29e6ceeb51bd58aa635ad91415e3f720c0e8
Win <synwe@yahoo.com>
2017-09-16 13:38:25 +0000

    Add option to generator runner to run it vs all events

--- chartflo/management/commands/gen.py
+++ chartflo/management/commands/gen.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 from django.core.management.base import BaseCommand
 from chartflo.apps import GENERATORS
 from .gencharts import get_changes_events, get_last_run_q, get_events_q
+from mqueue.models import MEvent
 
 
 class Command(BaseCommand):
@@ -14,7 +15,12 @@ class Command(BaseCommand):
         parser.add_argument('-q',
                             dest="quiet",
                             default=0,
-                            help='Quiet mode: ex: -q=1.',
+                            help='Quiet mode: ex: -q=1',
+                            )
+        parser.add_argument('-all',
+                            dest="all",
+                            default=0,
+                            help='Update for all instances: ex: -all=1',
                             )
 
     def handle(self, *args, **options):
@@ -22,7 +28,8 @@ class Command(BaseCommand):
         Run a generator
         """
         app = options["app"]
-        quiet = options["quiet"]
+        quiet = int(options["quiet"])
+        runall = int(options["all"])
         try:
             generator = GENERATORS[app]
         except:
@@ -32,6 +39,9 @@ class Command(BaseCommand):
         if quiet > 0:
             print("Running generator", app)
         last_run_q = get_last_run_q()
-        events_q = get_events_q()
-        events = get_changes_events(events_q, last_run_q)
-        generator(events)
+        if runall == 0:
+            events_q = get_events_q()
+            events_q = get_changes_events(events_q, last_run_q)
+        else:
+            events_q = MEvent.objects.all()
+        generator(events_q)

945a3f655822510d9c1d5ab5eeffe1461e6aaa0f
Win <synwe@yahoo.com>
2017-09-27 16:17:46 +0200

    Make the dashboard view available only for superusers

--- chartflo/views.py
+++ chartflo/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django.views.generic import TemplateView
+from django.http.response import Http404
 from .conf import ENGINE
 
 
@@ -11,6 +12,8 @@ class DashboardView(TemplateView):
 
     def dispatch(self, request, *args, **kwargs):
         self.slug = kwargs["slug"]
+        if not request.user.is_superuser is True:
+            return Http404
         return super(DashboardView, self).dispatch(request, *args, **kwargs)
 
     def get_template_names(self, *args, **kwargs):

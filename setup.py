47c06fbf6cdcf7e058ca674a268192eccafd1f44
Win <synwe@yahoo.com>
2017-10-12 13:16:39 +0200

    Prepare 0.2 release

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+version = __import__('chartflo').__version__
+
+setup(
+    name='chartflo',
+    packages=find_packages(),
+    version=version,
+    description='Charts for the lazy ones in Django',
+    author='synw',
+    author_email='synwe@yahoo.com',
+    url='https://github.com/synw/chartflo',
+    download_url='https://github.com/synw/chartflo/releases/tag/' + version,
+    keywords=["charts", "altair", "vega_lite"],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+    ],
+    zip_safe=False,
+    install_requires=[
+        'gencharts',
+        'goerr',
+        "blessings",
+    ],
+)

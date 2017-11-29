5c42abe265227ae6006db631958a0bc5632fc7c2
Win <synwe@yahoo.com>
2017-11-29 10:55:12 +0000

    Remove Chart and Number models

--- chartflo/admin.py
+++ chartflo/admin.py
@@ -1,21 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django.contrib import admin
-from .models import Chart, Number, Dashboard
-
-
-@admin.register(Number)
-class NumberAdmin(admin.ModelAdmin):
-    list_display = ("slug", "generator", "modelnames", "legend", "updated")
-    save_on_top = True
-    readonly_fields = ('updated',)
-
-
-@admin.register(Chart)
-class ChartAdmin(admin.ModelAdmin):
-    list_display = ("slug", "generator", "modelnames", "updated")
-    save_on_top = True
-    readonly_fields = ('updated',)
+from .models import Dashboard
 
 
 @admin.register(Dashboard)
--- chartflo/models.py
+++ chartflo/models.py
@@ -1,14 +1,6 @@
-# -*- coding: utf-8 -*-
-
-from goerr import err
 from django.db import models
-from django.conf import settings
-from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import Group
-from gencharts import ChartsGenerator
-from .utils import _write_file
-from .widgets import number_progress
 
 
 class Dashboard(models.Model):
@@ -25,6 +17,8 @@ class Dashboard(models.Model):
         u'Use the Altair rendering engine'))
     bokeh = models.BooleanField(default=True, verbose_name=_(
         u'Use the Bokeh rendering engine'))
+    generators = models.CharField(max_length=255, verbose_name=_(
+        u"Generators used by this dashboard"), help_text="List of apps: ex: mqueue,myapp")
 
     class Meta:
         verbose_name = _(u'Dashboard')
@@ -32,101 +26,3 @@ class Dashboard(models.Model):
 
     def __str__(self):
         return self.title
-
-
-class Number(models.Model):
-    slug = models.CharField(max_length=120, unique=True,
-                            verbose_name=_(u"Slug"))
-    value = models.IntegerField(verbose_name=_(u"Value"), default=0)
-    unit = models.CharField(max_length=120, blank=True,
-                            verbose_name=_(u"Unit"))
-    legend = models.CharField(
-        max_length=120, blank=True, verbose_name=_(u'Legend'))
-    html = models.TextField(blank=True, verbose_name=_(u'Html'))
-    updated = models.DateTimeField(
-        blank=True, null=True, verbose_name=_(u'Last update'))
-    modelnames = models.CharField(max_length=200, blank=True,
-                                  verbose_name=_(u"Associated models"),
-                                  help_text="List of model names: ex: User,Group"
-                                  )
-    generator = models.CharField(
-        max_length=120, blank=True, verbose_name=_(u"Generator"))
-    thresholds = models.CharField(max_length=255, blank=True,
-                                  verbose_name=_(u"Thresholds"))
-
-    class Meta:
-        verbose_name = _(u'Number')
-        verbose_name_plural = _(u'Numbers')
-
-    def __str__(self):
-        return self.legend
-
-    def generate(self, dashboard=None, color="green", icon=None, progress=None):
-        """
-        Generate data and save a panel number object in the database
-        """
-        # if progress is not None:
-        html = number_progress(self.value, self.legend,
-                               self.unit, self.thresholds, icon, color, progress=progress)
-        self.html = html
-        self.updated = timezone.now()
-        self.save()
-        _write_file(self.slug, self.html, "number", dashboard=dashboard)
-        if err.exists:
-            if settings.DEBUG is True:
-                err.trace()
-            else:
-                err.throw()
-
-
-class Chart(models.Model, ChartsGenerator):
-    name = models.CharField(max_length=120, verbose_name=_(u"Name"))
-    slug = models.CharField(max_length=120, unique=True,
-                            db_index=True, verbose_name=_(u"Slug"))
-    html = models.TextField(blank=True, verbose_name=_(u'Html'))
-    json = models.TextField(blank=True, verbose_name=_(
-        u'Vega Lite encoded json data'))
-    html_before = models.TextField(blank=True, verbose_name=_(u'Html before'))
-    html_after = models.TextField(blank=True, verbose_name=_(u'Html after'))
-    updated = models.DateTimeField(
-        blank=True, null=True, verbose_name=_(u'Last update'))
-    modelnames = models.CharField(max_length=200, blank=True,
-                                  verbose_name=_(u"Associated models"),
-                                  help_text="List of model names: ex: User,Group"
-                                  )
-    generator = models.CharField(
-        max_length=120, blank=True, verbose_name=_(u"Generator"))
-
-    class Meta:
-        verbose_name = _(u'Chart')
-        verbose_name_plural = _(u'Charts')
-
-    def __str__(self):
-        return self.name
-
-    def record(self, chart, slug, name="", generator="",
-               modelnames="", html_before="", html_after=""):
-        """
-        Generate data and save a chart object in the database
-        """
-        dataset = chart.to_json()
-        self.json = {}
-        try:
-            self.json = self._patch_json(dataset)
-        except Exception as e:
-            err.new(e)
-        try:
-            self.html = ""
-            if self.name:
-                self.html = "<h3>" + name + "</h3>"
-            self.html = html_before + self._json_to_html(slug, self.json) +\
-                self._json_to_html(slug, self.json) + html_after
-        except Exception as e:
-            err.new(e)
-        self.generator = generator
-        self.modelnames = modelnames
-        # save to db
-        try:
-            self.save()
-        except Exception as e:
-            err.new(e)

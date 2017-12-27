7359fae682afb6da9a9e9751828823682ef9d651
Win <synwe@yahoo.com>
2017-12-27 14:41:47 +0000

    Add a spinner while waiting for ajax calls to load

--- chartflo/templates/dashboards/app.js
+++ chartflo/templates/dashboards/app.js
@@ -1,6 +1,9 @@
 function loadDashboard(page, dashboard) {
 	var url = "/dashboards/page/"+dashboard+"/"+page+"/";
 	console.log("Getting", url);
+	var spinner = '<div class="text-center" style="width:100%;margin-top:5em;opacity:0.6">';
+	spinner = spinner + '<i class="fa fa-spinner fa-spin fa-5x fa-fw"></i>\n</div>';
+	$("#content").html(spinner);
 	axios.get(url).then(function (response) {
 		$('#content').html(response.data);
 	}).catch(function (error) {

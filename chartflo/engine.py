13b4ac8ef11aa3aeac65784ac6a119c8047e4057
Win <synwe@yahoo.com>
2017-12-30 12:49:11 +0000

    Add the new method

--- chartflo/engine.py
+++ chartflo/engine.py
@@ -1,7 +1,9 @@
+import pandas as pd
 from dataswim import DataSwim
+from .widgets import Widget
 
 
-class ChartFlo(DataSwim):
+class ChartFlo(DataSwim, Widget):
 
     def __repr__(self):
         """
@@ -9,6 +11,18 @@ class ChartFlo(DataSwim):
         """
         rows = str(len(self.df.columns))
         return '<Chartflo object - ' + rows + " >"
+
+    def new_(self, df=pd.DataFrame(), db=None, quiet=False):
+        """
+        Returns a new DataSwim instance from a dataframe
+        """
+        try:
+            cf2 = ChartFlo(df, db)
+        except Exception as e:
+            self.err(e, self.new_, "Can not set new instance")
+        if self.autoprint is True and quiet is False:
+            self.ok("A new instance was created")
+        return cf2
 
 
 cf = ChartFlo()

e986ee873dd62c0c333aa7530a1753cef09af39f
Win <synwe@yahoo.com>
2018-01-01 10:28:56 +0000

    Improve the js pages loading function

--- chartflo/templates/dashboards/app.js
+++ chartflo/templates/dashboards/app.js
@@ -1,24 +1,31 @@
-function loadDashboard(page, dashboard) {
+function loadDashboard(page, dashboard, destination) {
 	var url = "/dashboards/page/"+dashboard+"/"+page+"/";
 	console.log("Getting", url);
 	var spinner = '<div class="text-center" style="width:100%;margin-top:5em;opacity:0.6">';
 	spinner = spinner + '<i class="fa fa-spinner fa-spin fa-5x fa-fw"></i>\n</div>';
-	$("#content").html(spinner);
+	if ( destination === undefined) {
+    	destination = "#content"
+	}
+	$(destination).html(spinner);
 	axios.get(url).then(function (response) {
-		$('#content').html(response.data);
+		$(destination).html(response.data);
 	}).catch(function (error) {
 		console.log(error);
 	});
 	$(".sparkline-embeded").each(function () {
-	  var $this = $(this);
-	  $this.sparkline('html', $this.data());
+		//console.log("LOAD");
+		var $this = $(this);
+		$this.sparkline('html', $this.data());
+		console.log($this.data())
 	});
 }
 
 $(document).ready(function () {
 	loadDashboard("index", "{{ dashboard }}");
 	$(".sparkline").each(function () {
-	  var $this = $(this);
-	  $this.sparkline('html', $this.data());
+		//console.log("INIT");
+		var $this = $(this);
+		$this.sparkline('html', $this.data());
+		console.log($this.data())
 	});
 });
